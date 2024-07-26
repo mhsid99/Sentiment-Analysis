196e920ed19876f7b916032a601ff2c173870478
Hamza <hamzasiddiqui999@gmail.com>
2024-07-26 17:54:31 +0530

    updated scripts 4

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,2 +1,2 @@
 echo "Running container..."
-docker run --name flask_app -d -p 5000:5000 339713186666.dkr.ecr.us-east-1.amazonaws.com/sentiment-analysis:latest
+docker run --name sentiment-analysis -d -p 5000:5000 339713186666.dkr.ecr.us-east-1.amazonaws.com/sentiment-analysis:latest
