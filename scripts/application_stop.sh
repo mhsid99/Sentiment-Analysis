80d22dd9868c5fa9be7842ba0048905e0b060d58
Hamza <hamzasiddiqui999@gmail.com>
2024-07-26 17:13:41 +0530

    updated scripts

--- scripts/application_stop.sh
+++ scripts/application_stop.sh
@@ -1,7 +1,25 @@
 #!/bin/bash
 
-echo "This Script is used to stop already running docker container, remove them and remove the image as well"
+echo "This script stops and removes Docker containers and images"
 
-sudo docker stop $(sudo docker ps -q)
-sudo docker rm $(sudo docker ps -a -q)
-sudo docker rmi $(sudo docker images -q)
+# Get the container ID of the running container
+CONTAINER_ID=$(docker ps -q --filter "name=sentiment-analysis")
+
+if [ -n "$CONTAINER_ID" ]; then
+  echo "Stopping container..."
+  docker stop $CONTAINER_ID
+  echo "Removing container..."
+  docker rm $CONTAINER_ID
+else
+  echo "No running container found with the name flask_app"
+fi
+
+# Get the image ID of the Docker image
+IMAGE_ID=$(docker images -q 339713186666.dkr.ecr.us-east-1.amazonaws.com/sentiment-analysis:latest )
+
+if [ -n "$IMAGE_ID" ]; then
+  echo "Removing image..."
+  docker rmi $IMAGE_ID
+else
+  echo "No image found with the name 339713186666.dkr.ecr.us-east-1.amazonaws.com/sentiment-analysis:latest"
+fi
