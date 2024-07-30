63b455c4aa5eaa3eed1f19f83808159357d0243c
Hamza Siddiqui <hamzasiddiqui999@gmail.com>
2024-07-30 19:47:25 +0530

    Update application_start.sh

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,2 +1,2 @@
-echo "Running container..."
-docker run --name sentiment-analysis -d -p 5000:5000 339713186666.dkr.ecr.us-east-1.amazonaws.com/sentiment-analysis:latest
+echo "Running container...2nd try"
+docker run --name sentiment-analysis -d -p 5000:5000 339713186666:passw@339713186666.dkr.ecr.us-east-1.amazonaws.com/sentiment-analysis:latest
